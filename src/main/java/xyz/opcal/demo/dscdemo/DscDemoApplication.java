ea00a21075d9db043c432b18d5ed9388a2eec2c3
sing <sheng.work@outlook.com>
2024-04-08 11:59:58 +0800

    init

---
+++ src/main/java/xyz/opcal/demo/dscdemo/DscDemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.opcal.demo.dscdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/opcal/demo/dscdemo/DscDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.opcal.demo.dscdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
